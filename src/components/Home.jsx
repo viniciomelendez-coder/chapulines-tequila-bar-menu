9b23e5ffa10b4bc3f2989cd310cf5037b4561afa
viniciomelendez-coder <viniciomelendez@gmail.com>
2026-05-15 16:36:07 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ function CategoryIcon({ src, label }) {
       src={src}
       alt=""
       onError={() => setFailed(true)}
-      className="w-14 h-14 object-contain mb-3 opacity-95"
+      className="w-15 h-15 object-contain mb-3 opacity-95"
     />
   );
 }
